de55977e5bd96484a2d422d91ead3ccbdfd0daf4
“fredrickudom@gmail.com” <fredrickudom@gmail.comfredosky0705git config --global user.name FredrickUdomgit config --global user.email fredrickudom@gmail.com>
2022-06-18 01:21:51 +0100

    adding if statement to check and convert values to number

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
 
 function add(num1, num2){
+    // this will check the value and convert it to number
+    // if(typeof num1 === "number" && typeof num2 === "number"){
+    //     return num1 + num2;
+    // }else{
+    //     return +num1 + +num2;
+    // }
+
+    // this will concatenant both inputs values as string becuz of the .value
     return num1 + num2;
 }
 
